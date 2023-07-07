f1101df3fa5281338086ad7f049ef28dc78af3da
Shivvardhan <shivvardhan52@gmail.com>
2023-07-07 19:59:36 +0530

    added the login access

--- login_system/login.php
+++ login_system/login.php
@@ -1,33 +1,57 @@
 <?php
-if(isset($_POST['uname'])&& isset($_POST['password'])){
-    function validate($data){
+// MySQL database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "weeho";
 
-        $data =trim($data);
-        $data =stripslashes($data);
-        $data =htmlspecialchars($data);
-        return $data;
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['submit'])) {
+    // Check if form is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get user input from login form
+        $uname = $_POST["uname"];
+        $password = $_POST["password"];
+
+        // Prepare and execute SQL statement
+        $stmt = $conn->prepare("SELECT * FROM users WHERE uname = ?");
+        $stmt->bind_param("s", $uname);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Check if a row is returned
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+          
+
+            // Verify password
+            if ($password === $row["password"]) {
+                // Password is correct, login the user
+                session_start();
+                $_SESSION["user_id"] = $row["user_id"];
+                $_SESSION["uname"] = $row["uname"];
+
+                // Redirect to the user's dashboard or any other page
+                header("Location: ./dash.html");
+                exit();
+            } else {
+                // Password is incorrect
+                $error = "Invalid password.";
+            }
+        } else {
+            // No user found with the given username
+            $error = "User not found.";
+        }
     }
-  $uname =validate($_POST['uname']);
-  $pass =validate($_POST['password']);
-  
-  if(empty($uname)){
-    header("location: index.php?error=user Name is requested");
-    exit();
-  }
-  else if(empty($pass)){
-    header("location: index.php?error=password is requested");
-    exit();
-
-  }
-
-  else{
-    echo "Valid input";
-  }
 }
 
-else{
-    header("location:index.php");
-    exit();
-}
+// Close the database connection
+$conn->close();
 ?>
