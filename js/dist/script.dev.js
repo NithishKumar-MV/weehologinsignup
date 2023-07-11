a114e24d0fd580d1a7d7aecc4d371e54e46c7223
NithishKumar-MV <84956269+NithishKumar-MV@users.noreply.github.com>
2023-07-11 18:44:57 +0530

    add boot and css
    add

---
+++ js/dist/script.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function handlePersonalParties() {
+  // Perform the desired action for Personal Parties
+  // For example, show a modal or navigate to a specific page
+  alert("Personal Parties clicked!");
+}
+
+function handlePersonalCeremony() {
+  // Perform the desired action for Personal Ceremony
+  alert("Personal Ceremony clicked!");
+}
+
+function handleCorporateParties() {
+  // Perform the desired action for Corporate Parties
+  alert("Corporate Parties clicked!");
+}
+
+function handleCorporateCelebration() {
+  // Perform the desired action for Corporate Celebration
+  alert("Corporate Celebration clicked!");
+}
+
+function handleMICEEvents() {
+  // Perform the desired action for MICE Events
+  alert("MICE Events clicked!");
+}
+//# sourceMappingURL=script.dev.js.map
