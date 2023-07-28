db89ce26abe701e1b43e424c1450bb4118f54599
shahid ali <shahid576ali@gmail.com>
2023-07-28 21:24:30 +0530

    .

--- dashboard/changepassb.php
+++ dashboard/changepassb.php
@@ -8,7 +8,24 @@ if($password == $confirm_password){
     $sql="UPDATE `login_user` SET `password`='$password' WHERE email='$email'";
     if (mysqli_query($conn, $sql)) {
         
-        header("Location:dashboard.php");        
+        $result = mysqli_query($conn,"SELECT * FROM login_user WHERE email='$email' and password = '$password'");
+        $row  = mysqli_fetch_array($result);
+        if(is_array($row)) {
+        $_SESSION["id"] = $row['id'];
+        $_SESSION["name"] = $row['name'];
+        $_SESSION["email"] = $row['email'];
+        $_SESSION["phone"] = $row['phone'];
+        $_SESSION["city"] = $row['city'];
+        unset($_SESSION["message"]);
+        } else {
+         $message = "Invalid Username or Password!";
+         
+        }
+    
+        if(isset($_SESSION["id"])) {
+            header("Location:dashboard.php");
+            }
+        
 }
 }
 ?>
